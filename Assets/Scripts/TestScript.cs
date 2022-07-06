39239e10fe4ba62e5976c3d7c21e45e9f31b750f
Diego <diego_sousa.s@outlook.com>
2022-07-06 13:38:22 -0300

    Implementacao de camera em terceira pessoa

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -17,9 +17,9 @@ public class TestScript : MonoBehaviour
             transform.Translate(new Vector3(0,0,0.04f));
         if (Input.GetKey(KeyCode.S))
             transform.Translate(new Vector3(0, 0, -0.04f));
-        if (Input.GetKey(KeyCode.A))
-            transform.Translate(new Vector3(0.04f, 0, 0));
         if (Input.GetKey(KeyCode.D))
+            transform.Translate(new Vector3(0.04f, 0, 0));
+        if (Input.GetKey(KeyCode.A))
             transform.Translate(new Vector3(-0.04f, 0, 0));
     }
 }
