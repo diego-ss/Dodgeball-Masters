d5a1f99adbe4ab22a41167b149cd0d29d5f6d668
Diego <diego_sousa.s@outlook.com>
2022-07-07 13:58:08 -0300

    Melhorias e organização do PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,12 +3,17 @@
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    private float runSpeed;
     private Animator animator;
-    
+    private CapsuleCollider capsuleCollider;
+    private SphereCollider sphereCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = transform.GetComponent<Animator>();
+        capsuleCollider = transform.GetComponent<CapsuleCollider>();
+        sphereCollider = transform.GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -19,10 +24,12 @@ public class PlayerController : MonoBehaviour
 
     private void VerificaMovimento()
     {
+        //capturando A,D
         var inputX = Input.GetAxis("Horizontal");
+        //capturando W,S
         var inputZ = Input.GetAxis("Vertical");
-        var inputShift = Input.GetKey(KeyCode.LeftShift);
-        var inputControl = Input.GetKeyDown(KeyCode.LeftControl);
+
+
         var direcao = new Vector3(inputX, 0, inputZ);
 
         if(inputX != 0 || inputZ != 0)
@@ -32,30 +39,60 @@ public class PlayerController : MonoBehaviour
             camRot.z = 0;
             animator.SetBool("andarFrente", true);
 
-            if (inputShift)
-            {
-                transform.Translate(0, 0, speed * 1.3f * Time.deltaTime);
-                animator.SetBool("correr", true);
-            }
-            else
-            {
-                animator.SetBool("correr", false);
-                transform.Translate(0, 0, speed * Time.deltaTime);
-            }
-
-            if (inputControl)
-            {
-                animator.SetTrigger("rolar");
-                transform.Translate(0, 0, speed * 1.2f * Time.deltaTime);
-            }
-
+            VerificaAndarOuCorrer();
+            VerificaRolar();
 
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direcao) * camRot, 5 * Time.deltaTime);
         }
         else
         {
             animator.SetBool("andarFrente", false);
+        }
+    }
 
+    private void VerificaAndarOuCorrer()
+    {
+        runSpeed = 1.3f * speed;
+
+        //capturando shift
+        var inputShift = Input.GetKey(KeyCode.LeftShift);
+
+        //corrida (acontece mesmo andando, por isso o translate está aqui
+        if (inputShift)
+        {
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
+            animator.SetBool("correr", true);
+        }
+        else
+        {
+            animator.SetBool("correr", false);
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    private void VerificaRolar()
+    {
+        //capturando control
+        var inputControl = Input.GetKeyDown(KeyCode.LeftControl);
+
+        //rolagem
+        if (inputControl)
+        {
+            animator.SetTrigger("rolar");
+            transform.Translate(0, 0, speed * 1.2f * Time.deltaTime);
+        }
+
+        //desativando ou ativando os colliders de acordo com a necessidade
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("RollForward"))
+        {
+            sphereCollider.enabled = false;
+            capsuleCollider.enabled = true;
+        }
+        else
+        {
+            //caso esteja rolando, ativa um collider menor
+            sphereCollider.enabled = true;
+            capsuleCollider.enabled = false;
         }
     }
 
