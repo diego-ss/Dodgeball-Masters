247c7f41c3ca52ef0086a58f211c4e430e76cbe4
Diego <diego_sousa.s@outlook.com>
2022-07-07 13:33:33 -0300

    Mecanica de rolagem

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -21,6 +21,8 @@ public class PlayerController : MonoBehaviour
     {
         var inputX = Input.GetAxis("Horizontal");
         var inputZ = Input.GetAxis("Vertical");
+        var inputShift = Input.GetKey(KeyCode.LeftShift);
+        var inputControl = Input.GetKeyDown(KeyCode.LeftControl);
         var direcao = new Vector3(inputX, 0, inputZ);
 
         if(inputX != 0 || inputZ != 0)
@@ -28,10 +30,29 @@ public class PlayerController : MonoBehaviour
             var camRot = Camera.main.transform.rotation;
             camRot.x = 0;
             camRot.z = 0;
-            transform.Translate(0, 0, speed * Time.deltaTime);
             animator.SetBool("andarFrente", true);
+
+            if (inputShift)
+            {
+                transform.Translate(0, 0, speed * 1.3f * Time.deltaTime);
+                animator.SetBool("correr", true);
+            }
+            else
+            {
+                animator.SetBool("correr", false);
+                transform.Translate(0, 0, speed * Time.deltaTime);
+            }
+
+            if (inputControl)
+            {
+                animator.SetTrigger("rolar");
+                transform.Translate(0, 0, speed * 1.2f * Time.deltaTime);
+            }
+
+
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direcao) * camRot, 5 * Time.deltaTime);
-        } else
+        }
+        else
         {
             animator.SetBool("andarFrente", false);
 
