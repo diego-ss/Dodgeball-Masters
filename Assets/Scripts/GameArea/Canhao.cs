69a8e13378f9477e99b65f2049ad4afa83c04486
Diego <diego_sousa.s@outlook.com>
2022-07-14 15:38:49 -0300

    Implementação inicial da IA do canhão

--- Assets/Scripts/GameArea/Canhao.cs
+++ Assets/Scripts/GameArea/Canhao.cs
@@ -3,38 +3,78 @@ using UnityEngine;
 
 public class Canhao : MonoBehaviour
 {
+    [Header("Referencias")]
     public GameObject ballPrefab;
 
-    private GameObject ballOrigin;
-    private Bola ballScript;
-    private bool canShoot;
+    [Header("Parametros")]
+    public float rotateSpeed;
+    public float maxRotateAngle;
 
-    float initTime;
+    private GameObject ballOrigin;
+    private Transform cannonArmor;
+    private Bola ballScript;
+    private int ballToThrow;
+    private float lastShotTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        //captura o objeto de referência para instanciar a bola
         ballOrigin = transform.Find("Armor").Find("Cannon").Find("BallOrigin").gameObject;
-        initTime = Time.deltaTime;
+        //"mira" do canhão"
+        cannonArmor = transform.Find("Armor");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        var time = Mathf.Ceil(Time.timeSinceLevelLoad);
+        var rotY = Mathf.Ceil(cannonArmor.rotation.eulerAngles.y);
 
-        if (!canShoot && time != 0 && time % 3 == 0) 
-        {
-            var ball = Instantiate(ballPrefab);
-            ballScript = ball.GetComponent<Bola>();
-            ballScript.Capturar(ballOrigin, gameObject);
-            canShoot = true;
-        }
+        if ((rotY <= (Mathf.Ceil(maxRotateAngle + Mathf.Abs(rotateSpeed))) && rotY >= (Mathf.Ceil(maxRotateAngle))) || (rotY >= (360f - maxRotateAngle) && rotY <= 360f - maxRotateAngle + Mathf.Abs(rotateSpeed)))
+            rotateSpeed *= -1;
 
-        if (canShoot && time != 0 && time % 7 == 0)
+        cannonArmor.Rotate(new Vector3(0, rotateSpeed, 0));
+
+        RaycastHit hitInfo;
+        LayerMask layerMask = 1 << LayerMask.NameToLayer("Player");
+
+        if (ballToThrow == 0 && Time.timeSinceLevelLoad - lastShotTime > 3)
+            CarregarCanhao();
+
+        if (ballToThrow > 0 && Physics.Raycast(new Ray(ballOrigin.transform.position, ballOrigin.transform.forward), out hitInfo, Mathf.Infinity, layerMask))
         {
-            ballScript.Arremessar(new Vector3(3f, 0.2f, 0f));
-            canShoot = false;
+            lastShotTime = Time.timeSinceLevelLoad;
+            AtirarBola(hitInfo);
         }
+    }
+
+    private void AtirarBola(RaycastHit hitInfo)
+    {
+        var force = hitInfo.transform.position - ballOrigin.transform.position;
+        force.Normalize();
+        force *= 3f;
+        force.y += cannonArmor.position.y;
+        ballScript.Arremessar(force);
+        ballToThrow--;
+    }
+
+    private void CarregarCanhao()
+    {
+        var ball = Instantiate(ballPrefab, ballOrigin.transform.localPosition, ballOrigin.transform.rotation);
+        ballScript = ball.GetComponent<Bola>();
+        ballScript.Capturar(ballOrigin, gameObject);
+        ballToThrow++;
+    }
+
+    // LateUpdate is called once per frame
+    void LateUpdate()
+    {
+        //var time = Mathf.Ceil(Time.timeSinceLevelLoad);
+
+        //if (canShoot && time != 0 && time % 5 == 0)
+        //{
+
+        //    canShoot = false;
+        //}
+
     }
 }
